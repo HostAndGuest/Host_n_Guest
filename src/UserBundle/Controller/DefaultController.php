2a337883e0c73ffd23148c9bec73a4afd2a1451e
chahinebader <chahinebader93@gmail.com>
2017-02-08 17:51:54 +0100

    // add login / register

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
+    }
+
+    public function adminAction()
+    {
+        return new Response("ADMIN DETECTED !! ");
+    }
+
+    public function userAction()
+    {
+        return new Response("USER DETECTED !! ");
     }
 }
